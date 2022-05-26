0ddd0d4375110e80cfd1a5913a135a9109126369
Crystal <inkloud3@gmail.com>
2022-05-26 12:13:15 +0200

    uibuild

--- index.js
+++ index.js
@@ -63,18 +63,22 @@ app.post("/api/persons", (request, response, next) => {
     number: body.number,
   })
 
-  person.save().then((savedperson) => {
-    response.json(person)
-  })
+  person
+    .save()
+    .then((savedperson) => {
+      response.json(person)
+    })
+    .catch((error) => next(error))
 })
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson)
     })
@@ -86,6 +90,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/person.js
+++ models/person.js
@@ -11,8 +11,15 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    require: true,
+  },
 })
 
 personSchema.set("toJSON", {
